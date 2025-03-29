7746862affdf31d71d0ec27eebd2ce60f04dc041
almita-desai <almita.desai@gmail.com>
2025-03-29 13:31:48 +0530

    Add Pdf text extraction functionality

--- app.py
+++ app.py
@@ -1,5 +1,13 @@
 import os
 from flask import Flask,request,jsonify
+import fitz
+
+def read_pdf(filename):
+    doc=fitz.open(filename)
+    text=[page.get_text() for page in doc]
+
+    return '\n'.join(text).rstrip('\n')
+
 app=Flask(__name__)
 
 @app.route('/uploads',methods=["POST"])
@@ -9,9 +17,13 @@ def upload_pdf():
         return "invalid file",400
     
     os.makedirs("uploads",exist_ok=True)
-    file.save(os.path.join('uploads',file.filename))
-    return "file uploaded",200
-
+    filename=os.path.join('uploads',file.filename)
+    file.save(filename)
+    text=read_pdf(filename)
+    return jsonify({'text':text})
 
 if __name__=='__main__':
     app.run(debug=True)
+
+
+#make commit first-Add Pdf text extraction functionality
