8652650607a0c24662d122857a60dc20a024576f
almita-desai <almita.desai@gmail.com>
2025-03-29 15:42:24 +0530

    Implement text preprocessing

--- app.py
+++ app.py
@@ -1,12 +1,37 @@
 import os
 from flask import Flask,request,jsonify
+import string
 import fitz
+from nltk.corpus import stopwords
+import nltk
+import contractions
+nltk.download('stopwords')
+
 
 def read_pdf(filename):
     doc=fitz.open(filename)
     text=[page.get_text() for page in doc]
 
     return '\n'.join(text).rstrip('\n')
+
+def expand_contractions(text):
+    return contractions.fix(text)
+
+def remove_stopwords(text):
+    stop_words=set(stopwords.words('english'))
+    text=text.split()
+    text=[word for word in text if word not in stop_words]
+    return ' '.join(text)
+
+def clean_text(text):
+    text=expand_contractions(text)
+    text=text.lower()
+    text=text.translate(str.maketrans('','',string.punctuation))
+    text=' '.join(text.split())
+    text=text.strip()
+    text=remove_stopwords(text)
+    return text
+
 
 app=Flask(__name__)
 
@@ -19,11 +44,15 @@ def upload_pdf():
     os.makedirs("uploads",exist_ok=True)
     filename=os.path.join('uploads',file.filename)
     file.save(filename)
-    text=read_pdf(filename)
-    return jsonify({'text':text})
-
+    try:
+        text=read_pdf(filename)
+        cleaned_text=clean_text(text)
+        return jsonify({'text':cleaned_text})
+    except Exception as e:
+        return jsonify({'error': str(e)}),500
+    
+    finally:
+        if os.path.exists(filename):
+            os.remove(filename)
 if __name__=='__main__':
     app.run(debug=True)
-
-
-#make commit first-Add Pdf text extraction functionality
