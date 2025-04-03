6db0990290c2050df36fe73a6109fdfafb958a6a
almita-desai <almita.desai@gmail.com>
2025-04-03 23:11:54 +0530

    Add file size validation

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ nltk.download('stopwords')
 nltk.download('punkt')
 nltk.download('punkt_tab')
 
-
+MAX_FILE_SIZE = 5 * 1024 * 1024 
 def read_pdf(filename):
     doc=fitz.open(filename)
     text=[page.get_text() for page in doc]
@@ -47,9 +47,6 @@ def extract_top_sentences(text):
     top_sentences=summarizer(parser.document,sentence_count)
     return " ".join(str(sentence) for sentence in top_sentences)
 
-
-
-
 app=Flask(__name__)
 
 @app.route('/uploads',methods=["POST"])
@@ -57,6 +54,13 @@ def upload_pdf():
     file=request.files.get('file')
     if not file or not file.filename.endswith('.pdf'):
         return "invalid file",400
+    file.seek(0, os.SEEK_END)  # Move cursor to end of file
+    file_size = file.tell()
+    file.seek(0)  # Reset cursor
+    
+    if file_size > MAX_FILE_SIZE:
+        return jsonify({"error": "File size exceeds 5MB limit."}), 400
+
     
     os.makedirs("uploads",exist_ok=True)
     filename=os.path.join('uploads',file.filename)
