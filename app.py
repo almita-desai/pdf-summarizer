d1535552a4bd44b4ec605e1c898b037c1eee75af
almita-desai <almita.desai@gmail.com>
2025-04-05 23:51:51 +0530

    Add passive to active text conversion

--- app.py
+++ app.py
@@ -9,10 +9,12 @@ import contractions
 from sumy.summarizers.lsa import LsaSummarizer
 from sumy.nlp.tokenizers import Tokenizer
 from sumy.parsers.plaintext import PlaintextParser
+import spacy
+from lemminflect import getInflection
 
 nltk.download('stopwords')
 nltk.download('punkt')
-nltk.download('punkt_tab')
+nlp = spacy.load("en_core_web_sm")
 
 MAX_FILE_SIZE = 5 * 1024 * 1024 
 def read_pdf(filename):
@@ -47,6 +49,50 @@ def extract_top_sentences(text):
     top_sentences=summarizer(parser.document,sentence_count)
     return " ".join(str(sentence) for sentence in top_sentences)
 
+def detect_passive(text):
+    doc=nlp(text)
+    for token in doc:
+        if token.dep_=="auxpass":
+            return True
+    return False
+
+def rewrite_to_active(sentence):
+    doc=nlp(sentence)
+    subject=None
+    verb=None
+    agent=None
+    aux=None
+    for token in doc:
+        if token.dep_=='nsubjpass':
+            subject=token
+        elif token.dep_=='auxpass':
+            aux=token
+        elif token.dep_=='ROOT':
+            verb=token
+        elif token.dep_=='agent':
+            for child in token.children:
+                if child.dep_=='pobj':
+                    agent=child
+    if subject and verb and agent:
+        try:
+            active_verb=getInflection(verb.lemma_,tag='VBD')[0]
+        except:
+            active_verb=verb.text
+        return f"{agent.text.capitalize()} {active_verb} {subject.text}."
+    return sentence
+
+
+def apply_abstractive_summary(text):
+    sentences=sent_tokenize(text)
+    rewritten=[]
+    for sentence in sentences:
+        if detect_passive(sentence):
+            active_version=rewrite_to_active(sentence)
+            rewritten.append(active_version)
+        else:
+            rewritten.append(sentence)
+    return " ".join(rewritten)
+
 app=Flask(__name__)
 
 @app.route('/uploads',methods=["POST"])
@@ -54,9 +100,9 @@ def upload_pdf():
     file=request.files.get('file')
     if not file or not file.filename.endswith('.pdf'):
         return "invalid file",400
-    file.seek(0, os.SEEK_END)  # Move cursor to end of file
+    file.seek(0, os.SEEK_END)
     file_size = file.tell()
-    file.seek(0)  # Reset cursor
+    file.seek(0)  
     
     if file_size > MAX_FILE_SIZE:
         return jsonify({"error": "File size exceeds 5MB limit."}), 400
@@ -69,7 +115,8 @@ def upload_pdf():
         text=read_pdf(filename)
         # cleaned_text=clean_text(text)
         top_sentences=extract_top_sentences(text)
-        return jsonify({'text':top_sentences})
+        abstractive_summary=apply_abstractive_summary(top_sentences)
+        return jsonify({'text':abstractive_summary})
     except Exception as e:
         return jsonify({'error': str(e)}),500
     
