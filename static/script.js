6278db9841fa2f70dcee2fd8b354435f96fa886e
almita-desai <almita.desai@gmail.com>
2025-04-21 23:45:26 +0530

    Show file name after upload

--- static/script.js
+++ static/script.js
@@ -1,7 +1,7 @@
 const form=document.getElementById('form')
 const Input=document.getElementById('pdfFile')
 const result=document.getElementById('output')
-const error=document.getElementById('error')
+const filename=document.getElementById('fileName')
 
 form.addEventListener('submit',async(e)=>{
    e.preventDefault()
@@ -21,11 +21,21 @@ form.addEventListener('submit',async(e)=>{
          result.innerText=data.text
     }
     else{
-        error.innerText=data.error||"an error has occured"
+        result.innerText=data.error||"an error has occured"
     }
    }
 
    catch(err){
-    error.innerText='Error'
+    result.innerText='Error'
    }
 })
+
+const show_file_name=()=>{
+    let uploaded_file=Input.files[0]
+    if(uploaded_file){
+        filename.innerText=Input.files[0].name
+    }
+    else{
+        filename.innerText='No file chosen'
+    }
+}
